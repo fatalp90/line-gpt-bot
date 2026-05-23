dd4d82b020fdc005fe7f88339fabb4e50ca40833
fatalp90 <fatalp90@gmail.com>
2026-05-23 23:30:44 +0900

    Add files via upload

--- api/webhook.js
+++ api/webhook.js
@@ -17,6 +17,29 @@ export default async function handler(req, res) {
 
   for (const event of events) {
 
+    if (event.type === "join") {
+      await axios.post(
+        "https://api.line.me/v2/bot/message/reply",
+        {
+          replyToken: event.replyToken,
+          messages: [
+            {
+              type: "text",
+              text: "안녕하세요 :)"
+            }
+          ]
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}`,
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      continue;
+    }
+
     if (event.type !== "message") continue;
     if (event.message.type !== "text") continue;
 
